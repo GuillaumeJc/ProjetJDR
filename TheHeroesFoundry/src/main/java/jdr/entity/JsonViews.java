4cd91d0be209d4767915b2bb05863bb4430ba896
Guillaume <guillaumejacquet03@gmail.com>
2022-05-13 15:19:24 +0200

    Ajout des RestController

---
+++ TheHeroesFoundry/src/main/java/jdr/entity/JsonViews.java
@@ -0,0 +1,20 @@
+package jdr.entity;
+
+public class JsonViews {
+	// donnees qui ne posent pas de probleme
+	public static class Common {
+
+	}
+
+	public static class ModuleWithMatiere extends Common {
+
+	}
+
+	public static class ModuleWithFormateur extends ModuleWithMatiere {
+
+	}
+
+	public static class FormateurWithModules extends Common{
+		
+	}
+}
---
+++ TheHeroesFoundryFRONT/JDRFront/src/main/java/theHeroesFoundryFRONT/model/Adresse.java
@@ -0,0 +1,18 @@
+package theHeroesFoundryFRONT.model;
+
+public class Adresse {
+	String rue;
+
+	public Adresse() {
+
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+}
