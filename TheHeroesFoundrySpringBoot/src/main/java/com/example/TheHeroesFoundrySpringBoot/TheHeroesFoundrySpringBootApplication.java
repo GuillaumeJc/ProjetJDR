19669309bbd90e7ee27b6c413c64e70cab5bbdb7
Layla <adillayla@gmail.com>
2022-05-25 10:26:33 +0200

    Création nouveau projet SpringBoot
    Init fichiers

---
+++ TheHeroesFoundrySpringBoot/src/main/java/com/example/TheHeroesFoundrySpringBoot/TheHeroesFoundrySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.TheHeroesFoundrySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHeroesFoundrySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheHeroesFoundrySpringBootApplication.class, args);
+	}
+
+}
---
+++ TheHeroesFoundrySpringBoot/src/test/java/com/example/TheHeroesFoundrySpringBoot/TheHeroesFoundrySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TheHeroesFoundrySpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheHeroesFoundrySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
